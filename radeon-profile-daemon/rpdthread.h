7923bd62df3ab2c5c355e4085613895bfaa774fe
marazmista <marazmista@tuta.io>
2019-06-03 19:59:02 +0200

    version bump

--- radeon-profile-daemon/rpdthread.h
+++ radeon-profile-daemon/rpdthread.h
@@ -30,7 +30,7 @@
 #define SIGNAL_SHAREDMEM_KEY '6'
 #define SIGNAL_ALIVE '7'
 
-const QString appVersion = "20190309";
+const QString appVersion = "20190603";
 const QString serverSocketPath = "/run/radeon-profile-daemon-server";
 
 class rpdThread : public QThread
